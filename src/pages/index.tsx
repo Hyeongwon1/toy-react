8eedfe912f8df6eae53e9e31abb3d87b4f87b406
hwseo <hyeongwon1@naver.com>
2025-02-10 23:08:38 +0900

    넥스트js 서버사이드렌더링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,43 @@
 import style from './index.module.css';
 import SearchableLayout from "@/components/searchable-layout";
-import {ReactNode} from "react";
+import {ReactNode, useEffect} from "react";
 import books from '@/mock/book.json'
 import BookItem from '@/components/book-item'
+import {InferGetServerSidePropsType} from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  //컴포넌트보다 먼저실행
+  // const allBooks = await fetchBooks()
+  const [allBooks, recoBooks] = await Promise.all([
+      fetchBooks(),
+      fetchRandomBooks()
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks
+    },
+  }
+};
+
+export default function Home({allBooks, recoBooks}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(allBooks);
+  useEffect(() => {
+    console.log(window)
+  }, []);
   return (
       <div className={style.container}>
         <section>
           <h3>지금 추천하는 도서</h3>
-          {books.map((book) => (
+          {recoBooks.map((book) => (
               <BookItem key={book.id}{...book} />))}
         </section>
         <section>
           <h3>등록된 모든 도서</h3>
-          {books.map((book)=> (
+          {allBooks.map((book) => (
               <BookItem key={book.id} {...book}/>
           ))}
         </section>
