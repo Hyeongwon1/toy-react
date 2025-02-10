a69c2fa29a8af7c5ca342028b368e0787efddfb1
hwseo <hyeongwon1@naver.com>
2025-02-10 21:38:18 +0900

    넥스트js 클론코딩 커밋

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,16 @@
+import {useRouter} from "next/router";
+import {ReactNode} from "react";
+import SearchableLayout from "@/components/searchable-layout";
+import books from '@/mock/book.json'
+import BookItem from "@/components/book-item"
+
+export default function Page() {
+return<div>
+  {books.map((book) =>
+  <BookItem key={book.id}{...book}/>)}
+</div>
+}
+
+Page.getLayout =(page:ReactNode) => {
+  return<SearchableLayout>{page}</SearchableLayout>
+}
